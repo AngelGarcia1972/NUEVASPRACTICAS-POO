693eff2a7d03708fbbcb581050a39c77e1ae1326
AngelGarcia1972 <angeldaga.17@gmail.com>
2026-06-24 11:38:28 -0600

    ✨ Feat: create new components practica 2

---
+++ app/Http/Controllers/Api/CategoriaController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Categoria;
+use Illuminate\Http\Request;
+
+class CategoriaController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Categoria::all(), 200);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nombre' => 'required|string|max:150',
+            'slug'   => 'required|string|unique:categorias,slug',
+        ]);
+
+        $categoria = Categoria::create($data);
+        return response()->json($categoria, 201);
+    }
+
+    public function show(Categoria $categoria)
+    {
+        return response()->json($categoria, 200);
+    }
+
+    public function update(Request $request, Categoria $categoria)
+    {
+        $categoria->update($request->all());
+        return response()->json($categoria, 200);
+    }
+
+    public function destroy(Categoria $categoria)
+    {
+        $categoria->delete();
+        return response()->json(['mensaje' => 'Eliminado'], 200);
+    }
+}
